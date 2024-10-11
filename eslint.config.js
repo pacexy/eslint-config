adb8b7f1f62ac83adbb50d27278b6bb1917163c3
pacexy <pacexy@gmail.com>
2024-10-11 17:26:44 +0800

    fix(next): failed to load parser
    Error: Failed to load parser './parser.js' declared in ' » eslint-config-next/core-web-vitals » ...\eslint-config\node_modules\.pnpm\eslint-config-next@14.2.15_eslint@9.12.0_jiti@1.21.6__typescript@5.6.3\node_modules\eslint-config-next\index.js': Cannot find module 'next/dist/compiled/babel/eslint-parser'

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,12 @@ import { FlatCompat } from '@eslint/eslintrc'
 import pluginJs from '@eslint/js'
 import pluginReact from 'eslint-plugin-react'
 import globals from 'globals'
+import { isPackageExists } from 'local-pkg'
 import tseslint from 'typescript-eslint'
 
 const compat = new FlatCompat()
+
+const hasNext = isPackageExists('next')
 
 export default tseslint.config(
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
@@ -12,5 +15,5 @@ export default tseslint.config(
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  ...compat.extends('next/core-web-vitals'),
+  ...(hasNext ? compat.extends('next/core-web-vitals') : []),
 )
