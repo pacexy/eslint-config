c9ed384028fc617ec672359b2f83dabfcf5ee893
pacexy <pacexy@gmail.com>
2024-10-11 17:18:24 +0800

    feat: add eslint-config-next

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,16 @@
+import { FlatCompat } from '@eslint/eslintrc'
 import pluginJs from '@eslint/js'
 import pluginReact from 'eslint-plugin-react'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
-export default [
+const compat = new FlatCompat()
+
+export default tseslint.config(
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-]
+  ...compat.extends('next/core-web-vitals'),
+)
