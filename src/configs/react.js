d28df6b118a978385b5b3c69e620e3bb63ca3418
pacexy <pacexy@gmail.com>
2024-12-13 07:57:08 +0800

    feat: turn off react/react-in-jsx-scope

--- src/configs/react.js
+++ src/configs/react.js
@@ -2,15 +2,21 @@ import { fixupConfigRules } from '@eslint/compat'
 import { FlatCompat } from '@eslint/eslintrc'
 import pluginReact from 'eslint-plugin-react'
 import { isPackageExists } from 'local-pkg'
-import tseslint from 'typescript-eslint'
 
 const compat = new FlatCompat()
 
 const hasNext = isPackageExists('next')
 
-export const react = tseslint.config(
-  // @ts-expect-error TODO:
+/** @type {import('eslint').Linter.Config[]} */
+// @ts-expect-error TODO:
+export const react = [
   ...(hasNext
     ? fixupConfigRules(compat.extends('next/core-web-vitals'))
     : [pluginReact.configs.flat?.recommended]),
-)
+  {
+    name: 'pacexy/react',
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+]
