50f6c6157b72a0c579eefe5014021f792beb918b
pacexy <pacexy@gmail.com>
2025-07-15 09:52:41 +0800

    feat(sort): support `package.scripts`

--- src/configs/sort.js
+++ src/configs/sort.js
@@ -6,6 +6,19 @@ import {
 
 export const sort = [
   ...(await sortPackageJson()),
+  {
+    files: ['**/package.json'],
+    name: 'sort/package-json',
+    rules: {
+      'jsonc/sort-keys': [
+        'error',
+        {
+          pathPattern: '^scripts$',
+          order: { type: 'asc' },
+        },
+      ],
+    },
+  },
   ...sortTsconfig().map((c) => ({
     ...c,
     files: ['**/[jt]sconfig.json', '**/[jt]sconfig.*.json'],
